b1cd1814a905d1db7cf8a041e3b6176b2060c5c9
staehee <alinashin1030@gmail.com>
2026-09-01 18:23:26 +0900

    commit

---
+++ github_setup.R
@@ -0,0 +1,14 @@
+## Name, email
+usethis::use_git_config(user.name = "staehee", user.email = "alinashin1030@gmail.com")
+
+## setting: permanent storage
+credentials::credential_helper_set("manager")                 # Windows
+credentials::credential_helper_set("osxkeychain")             # Mac
+credentials::credential_helper_set("store")                   # Linux
+
+## create a personal access token for authentication:
+usethis::create_github_token() 
+## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
+
+## copy and paste your token and press enter
+credentials::set_github_pat()
